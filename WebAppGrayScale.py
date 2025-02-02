7969be2635bf7a05f67e9e849f3426e49c7bc726
tnsq <138233470+tnsq@users.noreply.github.com>
2025-02-02 23:31:02 +0530

    Update WebAppGrayScale.py

--- WebAppGrayScale.py
+++ WebAppGrayScale.py
@@ -2,14 +2,28 @@ import streamlit as st
 from PIL import Image
 #start the camera
 camera_image=st.camera_input(label="Camera")
+uploaded_image = st.file_uploader("Or upload an image", type=["jpg", "jpeg", "png"])
 
+
+image_source = camera_image if camera_image is not None else uploaded_image
 # create a pillow image instance
-if camera_image is not None:
-    img=Image.open(camera_image)
+if image_source is not None:
+    img=Image.open(image_source)
     gray_img = img.convert("L")
     st.image(gray_img)
 else:
     st.warning("Please capture an image to proceed.")
+
+
+# create a pillow image instance
+
+# img=Image.open(camera_image)
+
+# convert the pillow image to grayscale
+# gray_img=img.convert("L")
+
+# render the grayscale image on the webpage
+# st.image(gray_img)
 
 st.write("")
 st.write("")
